9f9b9ea8ce2a8734ded93582d282afe3e7a30432
Fernando Visbal <ferch5003@gmail.com>
2021-11-19 02:10:38 -0500

    feat: Tweet model done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   gravtastic
 
+  has_many :tweets
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
