6b7cd561491f89d61b3b3c104e7063270df7ac55
Fernando Visbal <ferch5003@gmail.com>
2021-11-26 14:50:00 -0500

    test: Added resources to UsersController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+RSpec.describe UsersController, type: :request do
+  let!(:user) { create(:user) }
+
+  describe 'GET #show' do
+    context 'when the user is not signed in' do
+      subject { get user_path(user.username) }
+
+      it { is_expected.to redirect_to(new_user_session_path) }
+    end
+
+    context 'when the user is signed in' do
+      before do
+        sign_in(user)
+      end
+
+      subject { get user_path(user.username) }
+
+      context 'with tweets' do
+        let!(:tweets) { create_list(:tweet, 3, user: user) }
+
+        before { subject }
+
+        it { expect(assigns(:tweets)).to match_array(tweets) }
+      end
+
+      context 'without tweets' do
+        before { subject }
+
+        it { expect(assigns(:tweets)).to match_array([]) }
+      end
     end
   end
-
 end
