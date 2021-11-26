c0eba2d6829cd17fe6a71c3a7bdc6dbdaa539d7d
Fernando Visbal <ferch5003@gmail.com>
2021-11-26 18:15:22 -0500

    fix: Social user on Users nested resources

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -1,9 +1,13 @@
 module Users
   class BaseController < ApplicationController
+    attr_reader :social_user
+
+    before_action :check_social_user
+
     private
 
-    def social_user
-      User.friendly.find(params[:user_id])
+    def check_social_user
+      @social_user = User.friendly.find(params[:user_id])
     end
   end
 end
